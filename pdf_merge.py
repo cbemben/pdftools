029022be2efdcc1a602cb1b0a76a5f8b5087b6ed
cbemben <cbemben04@gmail.com>
2020-10-19 16:01:19 -0400

    cleanup clutter and close connection

--- pdf_merge.py
+++ pdf_merge.py
@@ -23,19 +23,7 @@ class PdfMerge:
             fil = str(fil)
             self._obj.append(PyPDF2.PdfFileReader(fil, 'rb'))
         self._obj.write(outfile)
-
-    #read in pdf files
-    #pdf_obj = PyPDF2.PdfFileReader(stream='files/iris8.pdf')
-    #pdf_obj2 = PyPDF2.PdfFileReader(stream='files/iris4.pdf')
-
-    #use object to append
-    #pm = PyPDF2.PdfFileMerger()
-    #pm.append(pdf_obj)
-    #pm.append(pdf_obj2)
-
-    #write merged pdf
-    #pm.write(target directory)
-    #pm.close()
+        self._obj.close()
 
 if __name__=='__main__':
     test_dir = '/Users/FBIMAC/projectrepos/pdftools/files'
